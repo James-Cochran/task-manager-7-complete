8c018201e60638e947c81f912d1984c9c7767c16
James-Cochran <jameslikesfruit@gmail.com>
2024-07-02 09:31:36 -0400

    Initial commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-
+    def laundry?
+        if self.title == "laundry"
+            return true
+        elsif self.description == "laundry"
+            return true
+        else
+            return false
+        end
+    end
 end
